387dcc5463cbef1ebcb1dabd9aed9f6b22b0cfbb
Miroslav Jovic <jovicmiroslav88@gmail.com>
2023-06-28 00:18:47 +0200

    database updated

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google'
 
+import { connectToDB } from "@utils/database";
+
 const handler = NextAuth({
     providers: [
         GoogleProvider({
@@ -9,7 +11,20 @@ const handler = NextAuth({
         })
     ],
     async session({ session }) { },
-    async signIn({ profile }) { }
+    async signIn({ profile }) {
+        try {
+            await connectToDB();
+
+            //todo check if a user already exists
+
+            //todo if not, create a new user
+
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
 });
 
 export { handler as GET, handler as POST }
